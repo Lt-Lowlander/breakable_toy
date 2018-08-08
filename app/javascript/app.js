4b80b1618c1235baf80725c31e9c2e917bc7ac3d
Lt-Lowlander <aerocricket@gmail.com>
2018-08-08 13:36:17 -0400

    Beginning of a new project! -established framework -tweaked flash message

---
+++ app/javascript/app.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+
+    }
+  }
+
+  $(function(){
+        var flashDurationInSeconds = 5;
+        var flashContainerId = 'flash-messages';
+
+        function removeFlashMessages() {
+          $('#' + flashContainerId).remove();
+        }
+
+        setTimeout(removeFlashMessages, flashDurationInSeconds * 1000);
+      })
+
+  render() {
+    return(
+      <Router history={browserHistory}>
+        <Route path='/'>
+          <IndexRoute component={ProjectsIndexContainer} />
+        </Route>
+      </Router>
+    )
+  }
+}
+
+export default App;
