b6b34251d0fddff37a8af182c32b4c6b898d9ed9
Lt-Lowlander <aerocricket@gmail.com>
2018-08-09 12:08:36 -0400

    -refactored migrations -updated 404 page

--- db/migrate/20180808230804_create_projects.rb
+++ db/migrate/20180808230804_create_projects.rb
@@ -1,11 +1,12 @@
-class CreateProjectsTable < ActiveRecord::Migration[5.2]
+class CreateProjects < ActiveRecord::Migration[5.2]
   def change
-    create_table :projects_tables do |t|
+    create_table :projects do |t|
       t.string :name, null: false
       t.text :description, null: false
       t.integer :version_id, null: false
       t.string :photo_url, null: false
       t.integer :budget
+      t.string :topics
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -15,12 +15,13 @@ ActiveRecord::Schema.define(version: 2018_08_08_230804) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "projects_tables", force: :cascade do |t|
+  create_table "projects", force: :cascade do |t|
     t.string "name", null: false
     t.text "description", null: false
     t.integer "version_id", null: false
     t.string "photo_url", null: false
     t.integer "budget"
+    t.string "topics"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
