84f81546b16092196483ad3218fde689f7f8fb54
Lt-Lowlander <aerocricket@gmail.com>
2018-08-10 18:54:45 -0400

    -got the show pages online -got the index page online

--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -1,6 +1,3 @@
 class ProjectSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :version_id, :photo_url, :budget, :topics
-  has_many :equipment
-  has_many :materials
-  has_many :project_steps
 end
