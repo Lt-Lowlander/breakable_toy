41c5315f3266df6d7cf1b4da50bf3e5e356866ae
Lt-Lowlander <aerocricket@gmail.com>
2018-08-12 17:56:47 -0400

    wrangled with serializer, styled the project tiles

--- app/javascript/react/containers/ProjectShowContainer.js
+++ app/javascript/react/containers/ProjectShowContainer.js
@@ -10,6 +10,7 @@ class ProjectShowContainer extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      project: {},
       versions: [],
       equipment: [],
       materials: [],
@@ -19,9 +20,34 @@ class ProjectShowContainer extends Component {
 
   }
 
-  render(){
-    let message = "yo dog";
+  componentDidMount(){
+    fetch(`api/v1/projects/${this.props.params.id}`)
+      .then(response => {
+debugger
+      if (response.ok) {
+        return response;
+      } else {
+        let errorMessage = `${response.status} (${response.statusText})`,
+          error = new Error(errorMessage);
+        throw(error);
+      }
+    })
+    .then(response => response.json())
+    .then(body => {
+      this.setState({
+        equipment: body.project.equipment
+      })
+    })
+    .catch(error => console.error(`Error in venue show mount fetch: ${error.message}`));
+  }
 
+
+
+
+  render(){
+    const message = "yo dog";
+    const projectInfo = this.state.project;
+    let project
     return(
       <div>
         {message}
---
+++ app/serializers/equipment_serializer.rb
@@ -0,0 +1,4 @@
+class EquipmentSerializer < ActiveModel::Serializer
+  attributes :id, :user_id, :project_id, :tool_name
+  belongs_to :projects
+end
