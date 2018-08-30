75826c657c548ccd28187af451059c5949c002ea
Lt-Lowlander <aerocricket@gmail.com>
2018-08-30 16:33:26 -0400

    more user spec testing

--- spec/features/user_signs_out_spec.rb
+++ spec/features/user_signs_out_spec.rb
@@ -14,7 +14,7 @@ feature 'user signs out', %Q{
     user = FactoryBot.create(:user)
 
     visit new_user_session_path
-    
+
     fill_in 'Email', with: user.email
     fill_in 'Password', with: user.password
     fill_in 'Handle', with: user.handle
