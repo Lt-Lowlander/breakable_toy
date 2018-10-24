324d4f90b2e95b641076ca517f2df00affccaaa4
Lt-Lowlander <aerocricket@gmail.com>
2018-10-24 17:01:55 -0400

    cleaned up the show tile html and fixed up some styling

--- app/javascript/react/components/ProjectShowTile.js
+++ app/javascript/react/components/ProjectShowTile.js
@@ -7,7 +7,7 @@ let numeral;
 let ProjectShowTile = (props) => {
   if (props.ownership) {
     projectEdits =
-    <Link to={`/projects/${props.id}/edit`}>
+    <Link to={`/projects/${props.id}/edit`} className="edit-engage">
       <i className="far fa-edit edit-icon"></i>
     </Link>
   }
@@ -16,46 +16,36 @@ let ProjectShowTile = (props) => {
     <div className="project-show-tile">
       <div className="main-image">
         <img src={props.image} alt={props.name} className="project-show-pic"/>
-        <div className="project-show-info">
-          <div className="cell side-left">
-            <div className="title-group">
-              <div className="project-show-title">
-                <b>{props.name}</b>
-              </div>
-              <div className="">
-                {projectEdits}
-              </div>
-            </div>
-            <div className="ownership-group">
-              <div className="project-show-version">
-                Mark {props.numeral}
-              </div>
-              <div className="project-show-inventor">
-                by
-                <Link to={`/users/${props.userNum}`} className="user-title-path">
-                  <b>{props.user}</b>
-                </Link>
-              </div>
-            </div>
+      </div>
+      <div className="project-show-info">
+        {props.name}
+      </div>
+      <div className="bifurcation">
+        <div className="side-left ownership-group">
+          <div className="project-show-version">
+            Mark {props.numeral}
           </div>
-          <div className="side-right">
-            <OffSpringFormComponent
-              handle={props.user}
-              userId={props.userNum}
-              family={props.fam}
-              version={props.iteration + 1}
-              parent={props.id}
-              visitor={props.viewer}
+          <div className="project-show-inventor">
+            by
+            <Link to={`/users/${props.userNum}`} className="user-title-path">
+              {props.user}
+            </Link>
+          </div>
+        </div>
+        <div className="side-right">
+          {projectEdits}
+          <OffSpringFormComponent
+            handle={props.user}
+            userId={props.userNum}
+            family={props.fam}
+            version={props.iteration + 1}
+            parent={props.id}
+            visitor={props.viewer}
             />
-          </div>
         </div>
       </div>
-      <div className="descr">
-        <div className="smaller-div">
-          <b>
-            Description:
-          </b> {props.desc}
-        </div>
+      <div className="project-show-description">
+          <b>Description:</b><span className="buffer"></span>{props.desc}
       </div>
     </div>
   )
